2b304c8d67dc0eb589fac7eb44eb756315b296b6
Yogesh Sharma <ysharma@yotta.com>
2026-02-16 17:22:11 +0530

    Updates

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors: )")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return computer_choice
+
+choices = get_choices()
+print(choices)
+
+def check_win(player, computer):
+  print("You chose " + player + ", computer chose " + computer)
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
